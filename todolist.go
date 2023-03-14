8a4ced3cc58f15d6c427025845337fd534ee23d8
Leandro Arturi <lea.arturi@gmail.com>
2023-03-14 10:03:52 -0300

    Add and remove task to list

--- todolist.go
+++ todolist.go
@@ -8,24 +8,56 @@ type task struct {
 	completed   bool
 }
 
-func (t task) setCompleted() {
+func (t *task) setCompleted() {
 	t.completed = true
 }
 
-func (t task) setName(name string) {
+func (t *task) setName(name string) {
 	t.name = name
 }
 
-func (t task) setDescription(description string) {
+func (t *task) setDescription(description string) {
 	t.description = description
 }
 
+type taskList struct {
+	tasks []*task
+}
+
+func (tl *taskList) addToList(t *task) {
+	tl.tasks = append(tl.tasks, t)
+}
+
+func (tl *taskList) removeToList(index int) {
+	tl.tasks = append(tl.tasks[:index], tl.tasks[index+1:]...)
+}
+
 func main() {
-	t := task{
+
+	lista := &taskList{}
+
+	t1 := &task{
 		name:        "Completar curso Go",
-		description: "Url aqui",
+		description: "Hacerlo esta semana",
 		completed:   false,
 	}
 
-	fmt.Println(t)
+	t2 := &task{
+		name:        "Completar curso Ingles",
+		description: "Hacerlo este mes",
+		completed:   false,
+	}
+
+	t3 := &task{
+		name:        "Completar curso K8s",
+		description: "Hacerlo esta año",
+		completed:   false,
+	}
+
+	lista.addToList(t1)
+	lista.addToList(t2)
+	lista.addToList(t3)
+
+	fmt.Println(lista.tasks[0])
+
 }
